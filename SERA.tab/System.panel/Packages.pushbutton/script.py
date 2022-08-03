9f41e4a66ee78dd8ed32e8fd87edba36335fe109
Pete Heibel <peterjamesheibel@gmail.com>
2022-08-03 12:41:23 -0700

    adding Dynamo Packages reload tool

---
+++ SERA.tab/System.panel/Packages.pushbutton/script.py
@@ -0,0 +1,23 @@
+# """Reload pyRevit into new session."""
+# # -*- coding=utf-8 -*-
+# #pylint: disable=import-error,invalid-name,broad-except
+# from pyrevit import EXEC_PARAMS
+# from pyrevit import script
+# from pyrevit import forms
+# from pyrevit.loader import sessionmgr
+# from pyrevit.loader import sessioninfo
+
+import subprocess
+subprocess.call([r'\\local.serapdx.com\data\Support\ProgramData\AutoDesk\Dynamo\Revit\Deploy\Dynamo_Package Update.bat'])
+
+# res = True
+
+# if res:
+#     logger = script.get_logger()
+#     results = script.get_results()
+
+#     # re-load pyrevit session.
+#     logger.info('Reloading....')
+#     sessionmgr.reload_pyrevit()
+
+#     results.newsession = sessioninfo.get_session_uuid()
