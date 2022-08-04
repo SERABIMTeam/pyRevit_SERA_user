465f39d1d27381e7894acc746ef47db1270c03bb
Pete Heibel <peterjamesheibel@gmail.com>
2022-08-04 12:28:34 -0700

    updated Open Views script

--- SERA.tab/Tools.panel/Open Views.pushbutton/Open Views_script.py
+++ SERA.tab/Tools.panel/Open Views.pushbutton/Open Views_script.py
@@ -1,23 +1,6 @@
-import clr
+from pyrevit import revit
 
-clr.AddReference("RevitAPI")
-import Autodesk
-from Autodesk.Revit.DB import *
-from Autodesk.Revit.UI import *
-
-clr.AddReference("RevitServices")
-import RevitServices
-from RevitServices.Persistence import DocumentManager
-from RevitServices.Transactions import TransactionManager
-
-doc = DocumentManager.Instance.CurrentDBDocument
-uiapp = DocumentManager.Instance.CurrentUIApplication
-uiDoc = uiapp.ActiveUIDocument
-app = uiapp.Application
-
-ids = uiDoc.Selection.GetElementIds();
+ids = revit.uidoc.Selection.GetElementIds();
 
 for v in ids:
-	uiDoc.RequestViewChange(doc.GetElement(v))
-
-TransactionManager.Instance.ForceCloseTransaction()
+	revit.uidoc.RequestViewChange(revit.doc.GetElement(v))
