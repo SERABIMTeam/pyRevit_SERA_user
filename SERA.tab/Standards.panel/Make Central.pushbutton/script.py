29c17bb2687bc2b9edb347c70ef63a8c48598c8c
Pete Heibel <peterjamesheibel@gmail.com>
2023-05-05 12:25:34 -0700

    added if statement to add worksets based on type

--- SERA.tab/Standards.panel/Make Central.pushbutton/script.py
+++ SERA.tab/Standards.panel/Make Central.pushbutton/script.py
@@ -5,7 +5,13 @@ def create_worksets(d, names):
     for name in names:
         DB.Workset.Create(d, name)
 
-workset_names = ["UNITS", "X_A_SITE_RVT", "X_M_MECHANICAL_RVT", "X_S_STRUCTURAL_RVT"]
+building_workset_names = ["UNITS", "X_A_SITE_RVT", "X_M_MECHANICAL_RVT", "X_S_STRUCTURAL_RVT"]
+site_workset_names = ["X_A_BUILDING_RVT", "X_C_SURVEY_DWG"]
+# landscape_workset_names= []
+
+# each .rte file has a value for Project Information > Organization Description that includes a template text value (Building Template, Site Template, etc.)
+# this value is used to determine which standard worksets to add to the document
+templateType = revit.doc.ProjectInformation.get_Parameter(DB.BuiltInParameter.PROJECT_ORGANIZATION_DESCRIPTION).AsString()
 
 # enable worksharing if not already workshared
 if not revit.doc.IsWorkshared:
@@ -16,7 +22,12 @@ if not revit.doc.IsWorkshared:
     t = DB.Transaction(revit.doc, "Create Worksets")
     t.Start()
     # make your model changes here
-    create_worksets(revit.doc, workset_names)
+    if templateType == "Site Template":
+        create_worksets(revit.doc, site_workset_names)
+    # elif templateType == "Landscape Template":
+    #     create_worksets(revit.doc, landscape_workset_names)
+    else:
+        create_worksets(revit.doc, building_workset_names)
     t.Commit()
     
     # create a taskdialog to report next steps and offer help
