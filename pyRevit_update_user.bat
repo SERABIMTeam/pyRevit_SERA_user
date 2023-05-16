0ebfbb054fb39d044d1f33e634b7fd8b409a73df
Pete Heibel <peterjamesheibel@gmail.com>
2023-05-16 09:35:47 -0700

    moved user update script back to original location

--- pyRevit_update_user.bat
+++
@@ -1,5 +0,0 @@
-:: update the pyRevit extensions path to include the following folder
-pyrevit extensions paths add C:\ProgramData\Autodesk\pyRevit\extensions
-
-:: update the SERA_user.extension (%programdata%\Autodesk\pyRevit\extensions) with the latest GitHub repository
-pyrevit extensions update SERA_user
